d50d98c510eb38c6f007fa5a719f96e749096d17
Christa Hartsock <chartsock@pivotal.io>
2016-09-19 13:52:06 -0700

    Add scores endpoint with first data point
    Signed-off-by: Dan Junger <djunger@pivotal.io>

--- server.rb
+++ server.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'json'
 
-get '/services' do
-  content_type :json
+before do
+  content_type 'application/json'
+end
 
+get '/services' do
   {
     services: [
       {
@@ -22,3 +24,42 @@ get '/services' do
   }.to_json
 end
 
+get '/services/:service_slug/categories/:category_slug/scores' do
+  category_slug = params['category_slug']
+  scores_dictionary = {
+    'overall'=> [
+      {
+        'date' => '2016-09-15',
+        'value' => 88
+      }
+    ],
+    'marketing'=> [
+      {
+        'date' => '2016-09-15',
+        'value' => 95
+      }
+    ],
+    'finance'=> [
+      {
+        'date' => '2016-09-15',
+        'value' => 86
+      }
+    ],
+    'customer'=> [
+      {
+        'date' => '2016-09-15',
+        'value' => 89
+      }
+    ],
+    'operation'=> [
+      {
+        'date' => '2016-09-15',
+        'value' => 76
+      }
+    ],
+  }
+
+  {
+    scores: scores_dictionary[category_slug]
+  }.to_json
+end
