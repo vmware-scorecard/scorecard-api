fafa33b0083bf45b4bf85d5e6f93bc443bdc88fe
Christa Hartsock <chartsock@pivotal.io>
2016-09-20 15:34:47 -0700

    Add initial overall scores data for all services
    [#130313821]
    
    Signed-off-by: Annie Sing <asing@pivotal.io>

---
+++ data/scores.rb
@@ -0,0 +1,98 @@
+module Data::Scores
+  def scores_dictionary
+    {
+      'hybrid-cloud' => {
+        'overall' => [
+          {
+            'date' => '2016-09-26',
+            'value' => 86
+          },
+          {
+            'date' => '2016-09-19',
+            'value' => 89
+          },
+          {
+            'date' => '2016-09-12',
+            'value' => 88
+          },
+          {
+            'date' => '2016-09-05',
+            'value' => 85
+          },
+          {
+            'date' => '2016-08-29',
+            'value' => 80
+          },
+          {
+            'date' => '2016-08-22',
+            'value' => 73
+          },
+          {
+            'date' => '2016-08-15',
+            'value' => 72
+          },
+          {
+            'date' => '2016-08-08',
+            'value' => 75
+          },
+          {
+            'date' => '2016-08-01',
+            'value' => 78
+          },
+          {
+            'date' => '2016-07-25',
+            'value' => 84
+          },
+          {
+            'date' => '2016-07-18',
+            'value' => 82
+          },
+          {
+            'date' => '2016-07-11',
+            'value' => 80
+          }
+        ],
+        'marketing' => [
+          {
+            'date' => '2016-09-15',
+            'value' => 95
+          }
+        ],
+        'finance' => [
+          {
+            'date' => '2016-09-15',
+            'value' => 86
+          }
+        ],
+        'customer' => [
+          {
+            'date' => '2016-09-15',
+            'value' => 89
+          }
+        ],
+        'operation' => [
+          {
+            'date' => '2016-09-15',
+            'value' => 76
+          }
+        ]
+      },
+      'disaster-recovery' => {
+        'overall' => [
+          {
+            'date' => '2016-09-26',
+            'value' => 81
+          }
+        ]
+      },
+      'cloud-discover' => {
+        'overall' => [
+          {
+            'date' => '2016-09-26',
+            'value' => 93
+          }
+        ]
+      }
+    }
+  end
+end
--- server.rb
+++ server.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require 'json'
 require 'sinatra/cross_origin'
+require_relative './data/scores'
+
+include Data::Scores
 
 configure do
   enable :cross_origin
@@ -58,85 +61,10 @@ end
 
 get '/services/:service_slug/categories/:category_slug/scores' do
   category_slug = params['category_slug']
-  scores_dictionary = {
-    'overall' => [
-      {
-        'date' => '2016-09-26',
-        'value' => 86
-      },
-      {
-        'date' => '2016-09-19',
-        'value' => 89
-      },
-      {
-        'date' => '2016-09-12',
-        'value' => 88
-      },
-      {
-        'date' => '2016-09-05',
-        'value' => 85
-      },
-      {
-        'date' => '2016-08-29',
-        'value' => 80
-      },
-      {
-        'date' => '2016-08-22',
-        'value' => 73
-      },
-      {
-        'date' => '2016-08-15',
-        'value' => 72
-      },
-      {
-        'date' => '2016-08-08',
-        'value' => 75
-      },
-      {
-        'date' => '2016-08-01',
-        'value' => 78
-      },
-      {
-        'date' => '2016-07-25',
-        'value' => 84
-      },
-      {
-        'date' => '2016-07-18',
-        'value' => 82
-      },
-      {
-        'date' => '2016-07-11',
-        'value' => 80
-      }
-    ],
-    'marketing' => [
-      {
-        'date' => '2016-09-15',
-        'value' => 95
-      }
-    ],
-    'finance' => [
-      {
-        'date' => '2016-09-15',
-        'value' => 86
-      }
-    ],
-    'customer' => [
-      {
-        'date' => '2016-09-15',
-        'value' => 89
-      }
-    ],
-    'operation' => [
-      {
-        'date' => '2016-09-15',
-        'value' => 76
-      }
-    ],
-  }
+  service_slug = params['service_slug']
 
   {
-    scores: scores_dictionary[category_slug]
+    scores: scores_dictionary[service_slug][category_slug]
   }.to_json
 end
 
